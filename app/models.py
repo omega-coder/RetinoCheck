30c380a2d1167a9ca64e1319aa0e10de8a14a38a
CHERIEF Yassine <fy_cherief@esi.dz>
2020-08-04 19:33:16 +0100

    Added TestImage model

--- app/models.py
+++ app/models.py
@@ -2,7 +2,22 @@
 
 
 from django.db import models
-from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
 
+def f(instance, filename):
+    ext = filename.split(".")[-1]
+    if instance.pk:
+        return "{}/{}.{}".format(datetime.now().strftime("%Y"), instance.pk, ext)
+    else:
+        pass
+
+
+class TestImage(models.Model):
+    name = models.CharField(max_length=255, unique=True, null=False, primary_key=True)
+    image = models.ImageField(upload_to=f)
+    author = models.CharField(max_length=255, null=True, default="admin")
+    pred_class = models.CharField(max_length=10)
+    preds = models.CharField(max_length=255, null=True)
+    uploaded_at = models.DateTimeField(default=datetime.now)
+    real_label = models.CharField(max_length=10, null=True, blank=True, default="")
