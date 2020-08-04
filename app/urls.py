0c466396e1d80ee855be68554e4c2474567f1765
CHERIEF Yassine <fy_cherief@esi.dz>
2020-08-04 19:49:44 +0100

    Added image_labelize view

--- app/urls.py
+++ app/urls.py
@@ -5,11 +5,10 @@ from django.urls import path, re_path
 from app import views
 
 urlpatterns = [
-    # Matches any html file 
-    re_path(r'^.*\.html', views.pages, name='pages'),
-    path('profile/', views.profile, name="profile"),
-    path('results/', views.results, name='results'),
-    path('predict/', views.prediction, name='predict'),
-    # The home page
-    path('', views.index, name='home'),
+    re_path(r"^.*\.html", views.pages, name="pages"),
+    path("profile/", views.profile, name="profile"),
+    path("results/", views.results, name="results"),
+    path("predict/", views.prediction, name="predict"),
+    path("image/labelize/", views.image_labelize, name="labelize"),
+    path("", views.index, name="home"),
 ]
