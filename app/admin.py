5aa312060fdb50ff36e4be5b7eff4f8981cb6d28
CHERIEF Yassine <fy_cherief@esi.dz>
2020-08-11 17:01:13 +0100

    changed migrations for app

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,12 @@
 # -*- encoding: utf-8 -*-
 
 from django.contrib import admin
-from .models import TestImage
+from .models import TestImage, DRModel
+
 
 class TestImageAdmin(admin.ModelAdmin):
     pass
 
+
 admin.site.register(TestImage)
+admin.site.register(DRModel)
--- app/config.py
+++ app/config.py
@@ -3,5 +3,6 @@
 
 from django.apps import AppConfig
 
+
 class MyConfig(AppConfig):
     name = 'cfg'
