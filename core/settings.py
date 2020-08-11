bc2ee03c9d7fbea05954b3c8cf64f4391940b39a
CHERIEF Yassine <fy_cherief@esi.dz>
2020-08-11 16:58:50 +0100

    Formatted code

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,6 @@
 import os
 from decouple import config
 from unipath import Path
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -17,7 +16,8 @@ SECRET_KEY = config("SECRET_KEY", default="S#perS3crEt_1122")
 DEBUG = config("DEBUG", default=False)
 
 # load production server from .env
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", config("SERVER", default="127.0.0.1")]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1",
+                 config("SERVER", default="127.0.0.1")]
 
 # Application definition
 
@@ -46,7 +46,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = "core.urls"
 LOGIN_REDIRECT_URL = "home"  # Route defined in app/urls.py
 LOGOUT_REDIRECT_URL = "home"  # Route defined in app/urls.py
-TEMPLATE_DIR = os.path.join(BASE_DIR, "core/templates")  # ROOT dir for templates
+TEMPLATE_DIR = os.path.join(
+    BASE_DIR, "core/templates")  # ROOT dir for templates
 
 TEMPLATES = [
     {
@@ -70,7 +71,8 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": os.path.join(BASE_DIR, "db.sqlite3")}}
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3",
+                         "NAME": os.path.join(BASE_DIR, "db.sqlite3")}}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
