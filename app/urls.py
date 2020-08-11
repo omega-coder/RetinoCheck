03098dd1738a54bc9966dda391350e3ba105f4cd
CHERIEF Yassine <fy_cherief@esi.dz>
2020-08-11 17:00:51 +0100

    Refactored code for views

--- app/urls.py
+++ app/urls.py
@@ -1,17 +1,14 @@
 # -*- encoding: utf-8 -*-
 
 
-from django.urls import path, re_path
+from django.urls import path
 from app import views
 
 urlpatterns = [
-    re_path(r"^.*\.html", views.pages, name="pages"),
     path("profile/", views.profile, name="profile"),
     path("results/", views.results, name="results"),
     path("predict/", views.prediction, name="predict"),
     path("image/labelize/", views.image_labelize, name="labelize"),
-    
     path("prediction/correct", views.correct_prediction, name="correct_pred"),
-    
     path("", views.index, name="home"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,43 +1,29 @@
 # -*- encoding: utf-8 -*-
 
-
 import io
 import json
 from hashlib import sha1
 
 import requests
-from django import template
 from django.contrib.auth.decorators import login_required
 from django.db.utils import IntegrityError
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.template import loader
+from django.shortcuts import redirect, render
 from PIL import Image
 
-from .models import TestImage
 from .diagnosis import DRDiagnosis
+from .models import DRModel, TestImage
+from .util_funcs import (check_if_already_tested, check_if_rgba,
+                         convert_rgba_to_rgb,
+                         make_inMemoryUploadedFile_from_PIL_image)
 
 DIAG = DRDiagnosis()
+
+PREDICTION_API_ENDPOINT = "http://localhost:5000/api/v1/predict"
 
 
 @login_required(login_url="/login/")
 def index(request):
     return render(request, "index.html")
-
-
-@login_required(login_url="/login/")
-def pages(request):
-    context = {}
-    try:
-        load_template = request.path.split("/")[-1]
-        html_template = loader.get_template(load_template)
-        return HttpResponse(html_template.render(context, request))
-    except template.TemplateDoesNotExist:
-        html_template = loader.get_template("error-404.html")
-        return HttpResponse(html_template.render(context, request))
-    except Exception:
-        html_template = loader.get_template("error-500.html")
-        return HttpResponse(html_template.render(context, request))
 
 
 @login_required(login_url="/login/")
@@ -56,52 +42,58 @@ def prediction(request):
     if request.method == "POST":
         if request.FILES:
             try:
-                im = request.FILES["test-image"]
-                image_bytes = io.BytesIO(im.read())
+                image_file = Image.open(request.FILES["test-image"])
+                if check_if_rgba(image_file):
+                    image_file = convert_rgba_to_rgb(image_file)
+                image_bytes = io.BytesIO()
+                image_file.save(image_bytes, image_file.format)
                 data = {"model_name": "dr-latest", "model_type": 0}
-                im.seek(0)
-                image_pk = f"{sha1(im.read()).hexdigest()}"
-                instance = TestImage.objects.filter(name=image_pk).first()
-                if instance:
-                    if instance.real_label is not None:
-                        if instance.real_label != "":
-                            if int(instance.real_label) == int(instance.pred_class):
-                                context["message"] = f"Both doctor and model predicted the image as class {instance.real_label}"
-                            else:
-                                context["message"] = f"Doctor labelized image as class {instance.real_label}, but model predicted class {instance.pred_class}"        
-                        else:
-                            context["message"] = "Already prredicted this image! (Not yet labelized!)"   
-                    else:
-                        context["message"] = "Already prredicted this image! (Not yet labelized!)"
+                model = DRModel.objects.filter(name=data["model_name"]).first()
+                if model is None:
+                    return redirect(request, "predict.html")
+                image_pk = f"{sha1(image_bytes.getvalue()).hexdigest()}"
+                r_val, message, instance = check_if_already_tested(image_pk)
+                if r_val:
+                    context["message"] = message
                     context["instance"] = instance
-                    context["stade"] = DIAG.get(int(instance.pred_class), "stade")
-                    context["description"] = DIAG.get(int(instance.pred_class), "description")
-                    context["uistyle"] = DIAG.get(int(instance.pred_class), "uistyle")
+                    context["stade"] = DIAG.get(int(instance.pred_class),
+                                                "stade")
+                    context["description"] = DIAG.get(int(instance.pred_class),
+                                                      "description")
+                    context["uistyle"] = DIAG.get(int(instance.pred_class),
+                                                  "uistyle")
                     return render(request, "results.html", context=context)
-                resp = requests.post("http://127.0.0.1:5000/api/v1/predict/", data=data, files={"image": image_bytes})
-                im.seek(0)
+
+                resp = requests.post(PREDICTION_API_ENDPOINT,
+                                     data=data,
+                                     files={"image": image_bytes.getvalue()})
                 resp_json = resp.json()
-                preds = ",".join(
-                    ["{:.6f}".format(resp_json["predictions"][key]) for key in resp_json["predictions"].keys()]
-                )
-                instance = TestImage(
-                    name=image_pk,
-                    image=im,
-                    author=request.user.username,
-                    pred_class=json.loads(resp.text)["pred_class"],
-                    preds=preds,
-                )
+                preds = ",".join([
+                    "{:.6f}".format(resp_json["predictions"][key])
+                    for key in resp_json["predictions"].keys()
+                ])
+                image_in_mem = make_inMemoryUploadedFile_from_PIL_image(
+                    image_file)
+                instance = TestImage(name=image_pk,
+                                     image=image_in_mem,
+                                     author=request.user,
+                                     pred_class=json.loads(
+                                         resp.text)["pred_class"],
+                                     preds=preds,
+                                     model=model)
                 instance.save()
                 context["instance"] = instance
                 context["message"] = "success!"
-                context["stade"] = DIAG.get(int(instance.pred_class), "stade")
-                context["description"] = DIAG.get(int(instance.pred_class), "description")
-                context["uistyle"] = DIAG.get(int(instance.pred_class), "uistyle")
+                context["stade"] = DIAG.get(instance.pred_class, "stade")
+                context["description"] = DIAG.get(instance.pred_class,
+                                                  "description")
+                context["uistyle"] = DIAG.get(instance.pred_class, "uistyle")
                 return render(request, "results.html", context=context)
             except IntegrityError:
                 pass
-            except Exception as e:
-                print(type(e), e)
+            except Exception as error:
+                print(type(error), error)
+
     return render(request, "predict.html", context)
 
 
@@ -115,7 +107,6 @@ def image_labelize(request):
 def correct_prediction(request):
     context = {}
     if request.method == "POST":
-        #import pdb; pdb.set_trace()
         name = request.POST["name"]
         if request.POST["name"] and request.POST["reallabel"]:
 
@@ -124,13 +115,17 @@ def correct_prediction(request):
                 instance.real_label = request.POST["reallabel"]
                 instance.save()
                 if int(instance.real_label) == int(instance.pred_class):
-                    context["message"] = f"Both doctor and model predicted the image as class {instance.real_label}"
+                    context[
+                        "message"] = f"Both doctor and model predicted the image as class {instance.real_label}"
                 else:
-                    context["message"] = f"Doctor labelized image as class {instance.real_label}, but model predicted class {instance.pred_class}"        
+                    context[
+                        "message"] = f"Doctor labelized image as class {instance.real_label}, but model predicted class {instance.pred_class}"
                 context["instance"] = instance
                 context["stade"] = DIAG.get(int(instance.pred_class), "stade")
-                context["description"] = DIAG.get(int(instance.pred_class), "description")
-                context["uistyle"] = DIAG.get(int(instance.pred_class), "uistyle")
+                context["description"] = DIAG.get(int(instance.pred_class),
+                                                  "description")
+                context["uistyle"] = DIAG.get(int(instance.pred_class),
+                                              "uistyle")
                 return render(request, "results.html", context=context)
             else:
                 return redirect('predict')
