f51b355bd41d56ef0e0cffb6dcf78bc2f164438d
CHERIEF Yassine <fy_cherief@esi.dz>
2020-09-12 08:18:41 +0100

    Added view for generating reports

--- app/views.py
+++ app/views.py
@@ -1,13 +1,17 @@
 # -*- encoding: utf-8 -*-
 
+import os
 import io
 import json
 from hashlib import sha1
 
 import requests
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.db.utils import IntegrityError
 from django.shortcuts import redirect, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
 from PIL import Image
 
 from .diagnosis import DRDiagnosis
@@ -15,6 +19,7 @@ from .models import DRModel, TestImage
 from .util_funcs import (check_if_already_tested, check_if_rgba,
                          convert_rgba_to_rgb,
                          make_inMemoryUploadedFile_from_PIL_image)
+from datetime import datetime
 
 DIAG = DRDiagnosis()
 
@@ -23,7 +28,10 @@ PREDICTION_API_ENDPOINT = "http://localhost:5000/api/v1/predict"
 
 @login_required(login_url="/login/")
 def index(request):
-    return render(request, "index.html.j2")
+    context = {}
+    users = User.objects.all()
+    context["num_users"] = len(users)
+    return render(request, "index.html.j2", context=context)
 
 
 @login_required(login_url="/login/")
@@ -94,13 +102,22 @@ def prediction(request):
             except Exception as error:
                 print(type(error), error)
 
-    return render(request, "predict.html", context)
+    return render(request, "predict.html.j2", context)
 
 
 @login_required(login_url="/login/")
-def image_labelize(request):
+def image_labelize(request, page):
     context = {}
-    return render(request, "non-labelized.html.j2", context)
+    images_list = TestImage.objects.all()
+    paginator = Paginator(images_list, 6)
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        images = paginator.page(paginator.num_pages)
+    context["images"] = images
+    return render(request, "non-labelized.html.j2", context=context)
 
 
 @login_required(login_url="/login/")
@@ -108,8 +125,8 @@ def correct_prediction(request):
     context = {}
     if request.method == "POST":
         name = request.POST["name"]
-        if request.POST["name"] and request.POST["reallabel"]:
-
+        reallabel = request.POST["reallabel"]
+        if name and reallabel:
             instance = TestImage.objects.filter(name=name).first()
             if instance:
                 instance.real_label = request.POST["reallabel"]
@@ -126,8 +143,89 @@ def correct_prediction(request):
                                                   "description")
                 context["uistyle"] = DIAG.get(int(instance.pred_class),
                                               "uistyle")
-                return render(request, "results.html.j2", context=context)
+                return render(request, "corrected.html.j2", context=context)
             else:
                 return redirect('predict')
+    else:
+        return redirect('predict')
+
+
+@login_required(login_url="/login/")
+def result_name(request, name):
+    context = {}
+    r_val, message, instance = check_if_already_tested(name)
+    if r_val:
+        context["message"] = message
+        context["instance"] = instance
+        context["stade"] = DIAG.get(int(instance.pred_class),
+                                    "stade")
+        context["description"] = DIAG.get(int(instance.pred_class),
+                                            "description")
+        context["uistyle"] = DIAG.get(int(instance.pred_class),
+                                        "uistyle")
+        return render(request, "results.html.j2", context=context)
+    else:
+        return render(request, "index.html.j2")
+
+
+@login_required(login_url="/login/")
+def gen_report(request, name):
+    #check_if_pdf_exists()
+    instance = TestImage.objects.filter(name=name).first()
+    pdf_template_json = open("media/reports_data/template.json", "r", encoding='utf-8')
+    pdf_data_json = json.load(pdf_template_json)
+    if instance:
+        model_name = instance.model.name
+        preds = list(map(float, instance.preds.split(',')))
+        pred_class = instance.pred_class
+        real_label = instance.real_label
+        eye_orientation = instance.eye_orientation
+        author = instance.author.username
+        generated_at = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        pdf_data_json["rapport_gentime"] = generated_at
+        pdf_data_json["pred_class"] = str(pred_class)
+        if real_label:
+            pdf_data_json["real_label"] = real_label
+        if eye_orientation:
+            pdf_data_json["eye_orientation"] = eye_orientation
+        pdf_data_json["model_name"] = model_name
+        pdf_data_json["author"] = author
+        diagnostic = DIAG.get(int(instance.pred_class), "description")
+        pdf_data_json["diagnostic"] = diagnostic
+        pdf_data_json["stade"] = DIAG.get(int(instance.pred_class), "stade")
+        pdf_data_json["db_name"] = instance.name
+
+
+        pdf_data_json["top_holdings"][0]["amount"] = str(preds[0])
+        pdf_data_json["top_holdings"][1]["amount"] = str(preds[1])
+        pdf_data_json["top_holdings"][2]["amount"] = str(preds[2])
+        pdf_data_json["top_holdings"][3]["amount"] = str(preds[3])
+        pdf_data_json["top_holdings"][4]["amount"] = str(preds[4])
+
+
+
+        # CREATE JSON FILE FOR REPORT
+        pdf_template_json.close()
+        json_pdf_new = open(f"media/reports_data/{instance.name}.json", "w")
+
+        # DUMP jSON IN FILE
+
+        json.dump(pdf_data_json, json_pdf_new)
+        json_pdf_new.close()
+
+        # PDF GENERATION
+        #command = "ls"
+        command = f'cd report; python3 gen_factsheet.py ../media/reports_data/{instance.name}.json'
+        os.system(command)
+
+
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="{instance.name}.pdf"'
+        f = open(
+            f'media/reports/{instance.name}.pdf',
+            'rb')
+        data = f.read()
+        response.write(data)
+        return response
     else:
         return redirect('predict')
