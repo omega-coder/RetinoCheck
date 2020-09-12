02cc76b05bc41a7cad820c5abf5759ac183d47df
CHERIEF Yassine <fy_cherief@esi.dz>
2020-09-12 08:18:03 +0100

    Added urls for reporting and image labelization

--- app/urls.py
+++ app/urls.py
@@ -7,8 +7,10 @@ from app import views
 urlpatterns = [
     path("profile/", views.profile, name="profile"),
     path("results/", views.results, name="results"),
+    path("result/<str:name>", views.result_name, name="result_name"),
     path("predict/", views.prediction, name="predict"),
-    path("image/labelize/", views.image_labelize, name="labelize"),
+    path("image/labelize/<int:page>/", views.image_labelize, name="labelize"),
     path("prediction/correct", views.correct_prediction, name="correct_pred"),
+    path("report/generate/<str:name>/", views.gen_report, name="genreport"),
     path("", views.index, name="home"),
 ]
